ee841d7beacfb61f13d6fd5ce4dafb7a554508de
Saikat <saikat344@gmail.com>
2020-11-18 16:43:25 -0800

    hello world

---
+++ docs/tapkulibrary/demo/Classes/CalendarDayViewController.h
@@ -0,0 +1,16 @@
+//
+//  CalendarDayViewController.h
+//  Demo
+//
+//  Created by Devin Ross on 3/16/13.
+//
+//
+
+@import tapku;
+@import UIKit;
+
+@interface CalendarDayViewController : TKCalendarDayViewController
+
+@property (nonatomic,strong) NSArray *data;
+
+@end
